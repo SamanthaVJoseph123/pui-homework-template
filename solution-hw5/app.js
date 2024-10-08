1bce23ca0665411a9983b0442f22d5a07774d4d4
SamanthaVJoseph123 <svjoseph@andrew.cmu.edu>
2024-10-08 11:17:00 -0400

    Transferred HW4

--- solution-hw5/app.js
+++ solution-hw5/app.js
@@ -1,1 +1,80 @@
+let glazingElement = document.querySelector('#glaze-select');
+let sizeElement = document.querySelector('#pack-select');
 
+let baseBunPrice = 2.49;
+// let glazingPrice = 0;
+// let packSizePrice = 0;
+
+// let constValue = 0;
+let totalPrice = 0;
+let glazeValue = 0;
+let sizeValue = 1;
+
+let glazingOptions = [
+    {
+        glazeName: 'Keep Original',
+        glazePrice: 0,
+    },
+    {
+        glazeName: "Sugar Milk",
+        glazePrice: 0,
+    },
+    {
+        glazeName: "Vanilla Milk",
+        glazePrice: 0.5,
+    },
+    {
+        glazeName: "Double Chocolate",
+        glazePrice: 1.50,
+    },
+  ];
+
+  let sizeOptions = [
+    {
+        sizeName: '1',
+        sizePrice: 1,
+    },
+    {
+        sizeName: "3",
+        sizePrice: 3,
+    },
+    {
+        sizeName: "6",
+        sizePrice: 5,
+    },
+    {
+        sizeName: "12",
+        sizePrice: 10,
+    },
+  ];
+
+// Glaze Options
+for (let i = 0; i < glazingOptions.length; i++) {
+    let option = document.createElement('option');
+    option.text = glazingOptions[i].glazeName;
+    option.value = glazingOptions[i].glazePrice;
+    glazingElement.add(option);
+}
+
+// Size Options
+for (let i = 0; i < sizeOptions.length; i++) {
+    let option = document.createElement('option');
+    option.text = sizeOptions[i].sizeName;
+    option.value = sizeOptions[i].sizePrice;
+    sizeElement.add(option);
+}
+
+function glazingChange(element){
+    glazeValue = parseFloat(element.value);
+    updateTotalPrice(glazeValue, sizeValue);
+}
+
+function sizeChange(element){
+    sizeValue = parseFloat(element.value);
+    updateTotalPrice(glazeValue, sizeValue);
+}
+
+function updateTotalPrice(glazeValue, sizeValue) {
+    totalPrice = (baseBunPrice + glazeValue) * sizeValue;
+    document.querySelector('#total-price').innerText = "$" + totalPrice.toFixed(2);
+}
--- solution-hw5/details.js
+++ solution-hw5/details.js
@@ -1,1 +1,49 @@
+class Roll{
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing = rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
 
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll')
+
+const selectedRoll = rolls[rollType];
+
+let cart = [];
+
+const rollName = rollType + " " + "Cinnamon Roll";
+const rollPrice = selectedRoll.basePrice;
+const rollImage = "../assets/products/" + selectedRoll.imageFile;
+
+const rollHeaderElement = document.querySelector('.center-text-header');
+rollHeaderElement.innerText = rollName;
+
+const rollImageElement = document.querySelector('.item-page-box');
+rollImageElement.src = rollImage;
+
+const rollPriceElement = document.querySelector('#total-price');
+rollPriceElement.innerText = "$" + rollPrice;
+baseBunPrice = rollPrice;
+
+function addRolltoCart(){
+    // let glazeType = glazingElement.text;
+    // let sizeType = sizeElement.text;
+    let glazeType = glazingElement.options[glazingElement.selectedIndex].text;
+    let sizeType = sizeElement.options[sizeElement.selectedIndex].text;
+
+    addedRoll = new Roll(rollType,glazeType,sizeType,baseBunPrice);
+    cart.push(addedRoll);
+    console.log(cart);
+}
+
+//glazingElement.selectedIndex -> gets the index of the selected option in the glazingElement dropdown
+//glazingElement.options -> gets the <options> within the glazingElement
+//.text lets us edit single out the text
+
+// RESOURCES I USED: 
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/selectedIndex - to learn about the selectedIndex property
+// https://playcode.io/javascript/classes - to learn about creating new instance
