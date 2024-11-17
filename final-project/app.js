6309f444270087bbb1d6cbf82e25397ac20c6888
SamanthaVJoseph123 <svjoseph@andrew.cmu.edu>
2024-11-17 17:22:08 -0500

    Update

--- final-project/app.js
+++ final-project/app.js
@@ -1,7 +1,6 @@
 class Notecard {
-    constructor(imageURL, title, body) {
+    constructor(title, body) {
         console.log('Create a new notecard!');
-        this.noteImageURL = imageURL;
         this.noteTitle = title;
         this.noteBody = body;
         this.element = document.querySelector(elementID);
@@ -18,21 +17,18 @@ class Notecard {
 // notecardOne.noteBody;
 
 const notecardOne = new Notecard(
-  'assets/warhol-frog.png',
   'This is the First Note',
   'Here is some body text for the first note.',
   '#notecard-one'
 )
 
 const notecardTwo = new Notecard(
-  'assets/warhol-orangutan.png',
   'This is the Second Note',
   'And here is some body text for the second note! What could be next?',
   '#notecard-two'
 )
 
 const notecardThree = new Notecard(
-  'assets/warhol-eagle.png',
   'This is the Third Note',
   'Yep, you guessed it, here is some body text for the third note.',
   '#notecard-three'
