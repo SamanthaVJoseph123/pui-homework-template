1ddb7b85a5815e1d1091b73c90124146f7eaa159
SamanthaVJoseph123 <svjoseph@andrew.cmu.edu>
2024-11-17 18:11:21 -0500

    update

--- final-project/app.js
+++ final-project/app.js
@@ -1,10 +1,13 @@
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+
 class Notecard {
-    constructor(title, body) {
-        console.log('Create a new notecard!');
-        this.noteTitle = title;
-        this.noteBody = body;
-        this.element = document.querySelector(elementID);
-    }
+  constructor(imageURL, title, body) {
+    this.noteImageURL = imageURL;
+    this.noteTitle = title;
+    this.noteBody = body;
+    this.element = null;
+  }
 }
 
 // const notecardOne= new Notecard('test-image', 'test-title', 'test-body');
@@ -16,20 +19,48 @@ class Notecard {
 // notecardOne.noteTitle;
 // notecardOne.noteBody;
 
-const notecardOne = new Notecard(
-  'This is the First Note',
-  'Here is some body text for the first note.',
-  '#notecard-one'
-)
+const notecardSet = new Set();
 
-const notecardTwo = new Notecard(
-  'This is the Second Note',
-  'And here is some body text for the second note! What could be next?',
-  '#notecard-two'
-)
+function addNewNote(imageURL, title, body){
+  const notecard = new Notecard(imageURL, title, body);
+  notecardSet.add(notecard);
+  return notecard;
+}
 
-const notecardThree = new Notecard(
-  'This is the Third Note',
-  'Yep, you guessed it, here is some body text for the third note.',
-  '#notecard-three'
-)
+const notecardOne = addNewNote(
+  "../assets/finalProjectAssets/bookCover.jpg",
+  "The first note title",
+  "The first note body"
+);
+
+const notecardTwo = addNewNote(
+  "../assets/finalProjectAssets/bookCover.jpg",
+  "The second note title",
+  "The second note body"
+);
+
+for (const notecard of notecardSet){
+  console.log(notecard);
+  createElement(notecard);
+}
+
+function createElement(notecard){
+  const template = document.querySelector('#notecard-template');
+  const clone = template.content.cloneNode(true);
+  notecard.element = clone.querySelector('.notecard-container');
+
+  const notecardListElement = document.querySelector('#notecard-list');
+  notecardListElement.append(clone);
+
+  updateElement(notecard);
+}
+
+function updateElement(notecard){
+  const noteImageElement = notecard.element.querySelector('.notecard-image');
+  const noteTitleElement = notecard.element.querySelector('.notecard-title');
+  const noteBodyElement = notecard.element.querySelector('.notecard-body');
+
+  noteImageElement.src = notecard.noteImageURL;
+  noteTitleElement.innerText = notecard.noteTitle;
+  noteBodyElement.innerText = notecard.noteBody;
+}
