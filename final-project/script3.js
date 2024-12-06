abb893f7e1691fe4ea43b350ce1e34ff4a92256f
SamanthaVJoseph123 <svjoseph@andrew.cmu.edu>
2024-12-05 20:04:56 -0500

    updates

--- final-project/script3.js
+++ final-project/script3.js
@@ -10,7 +10,8 @@ const x = JSON.parse(storedData);
 if (x[noteIndex]){
     document.querySelector(".post-title").textContent = x[noteIndex].title;
     document.querySelector(".post-topic").textContent = x[noteIndex].topic;
-    document.querySelector(".post-image").textContent = x[noteIndex].imageURL;
+    document.querySelector(".post-image").src = x[noteIndex].imageURL;
+    document.querySelector(".post-image").alt = x[noteIndex].imageText;
     document.querySelector(".post-full-body").textContent = x[noteIndex].postBody;
 } else {
     console.error("roll type not found")
