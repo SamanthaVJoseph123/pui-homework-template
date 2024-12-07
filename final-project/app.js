a6a85b6a360630f6e99bb0952ef5b71a624933cd
SamanthaVJoseph123 <svjoseph@andrew.cmu.edu>
2024-12-06 19:18:47 -0500

    final SAVE before crazy changes

--- final-project/app.js
+++ final-project/app.js
@@ -1,76 +1,78 @@
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
+// THIS CODE IS NOT NECESSARY
 
-class Notecard {
-  constructor(imageURL, imageText, title, summaryBody, postBody, topic) {
-    this.noteImageURL = imageURL;
-    this.noteImageText = imageText;
-    this.noteTitle = title;
-    this.noteSummaryBody = summaryBody;
-    this.notePostBody = postBody;
-    this.noteTopic = topic;
-    this.element = null;
-  }
-}
+// const queryString = window.location.search;
+// const params = new URLSearchParams(queryString);
 
-// const notecardOne= new Notecard('test-image', 'test-title', 'test-body');
-// "this" keyword refers to the Notecard object that the constructor function creates
+// class Notecard {
+//   constructor(imageURL, imageText, title, summaryBody, postBody, topic) {
+//     this.noteImageURL = imageURL;
+//     this.noteImageText = imageText;
+//     this.noteTitle = title;
+//     this.noteSummaryBody = summaryBody;
+//     this.notePostBody = postBody;
+//     this.noteTopic = topic;
+//     this.element = null;
+//   }
+// }
 
-// EXAMPLE:
-// const notecardOne = new Notecard('test-image', 'test-title', 'test-body');
-// notecardOne.noteImageURL;
-// notecardOne.noteTitle;
-// notecardOne.noteBody;
+// // const notecardOne= new Notecard('test-image', 'test-title', 'test-body');
+// // "this" keyword refers to the Notecard object that the constructor function creates
 
-const notecardSet = new Set();
+// // EXAMPLE:
+// // const notecardOne = new Notecard('test-image', 'test-title', 'test-body');
+// // notecardOne.noteImageURL;
+// // notecardOne.noteTitle;
+// // notecardOne.noteBody;
 
-function addNewNote(imageURL, imageText, title, summaryBody, postBody, topic){
-  const notecard = new Notecard(imageURL, imageText, title, summaryBody, postBody, topic);
-  notecardSet.add(notecard);
-  return notecard;
-}
+// const notecardSet = new Set();
 
-const notecardOne = addNewNote(
-  "../assets/finalProjectAssets/bookCover.jpg",
-  "Alternate Text for post 1",
-  "The first note title",
-  "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
-  "This is the postBody for Post 1 lorem ipsum",
-  "Adventure"
-);
+// function addNewNote(imageURL, imageText, title, summaryBody, postBody, topic){
+//   const notecard = new Notecard(imageURL, imageText, title, summaryBody, postBody, topic);
+//   notecardSet.add(notecard);
+//   return notecard;
+// }
 
-const notecardTwo = addNewNote(
-  "../assets/finalProjectAssets/bookCover.jpg",
-  "Alternate Text for post 2",
-  "The second note title",
-  "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
-  "This is the postBody for Post 1 lorem ipsum",
-  "Romance"
-);
+// const notecardOne = addNewNote(
+//   "../assets/finalProjectAssets/bookCover.jpg",
+//   "Alternate Text for post 1",
+//   "The first note title",
+//   "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+//   "This is the postBody for Post 1 lorem ipsum",
+//   "Adventure"
+// );
 
-for (const notecard of notecardSet){
-  console.log(notecard);
-  createElement(notecard);
-}
+// const notecardTwo = addNewNote(
+//   "../assets/finalProjectAssets/bookCover.jpg",
+//   "Alternate Text for post 2",
+//   "The second note title",
+//   "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+//   "This is the postBody for Post 1 lorem ipsum",
+//   "Romance"
+// );
 
-function createElement(notecard){
-  const template = document.querySelector('#notecard-template');
-  const clone = template.content.cloneNode(true);
-  notecard.element = clone.querySelector('.notecard-container');
-  const notecardListElement = document.querySelector('#notecard-list');
-  notecardListElement.append(clone);
-  updateElement(notecard);
-}
+// for (const notecard of notecardSet){
+//   console.log(notecard);
+//   createElement(notecard);
+// }
 
-function updateElement(notecard){
-  const noteImageElement = notecard.element.querySelector('.notecard-image');
-  const noteTitleElement = notecard.element.querySelector('.notecard-title');
-  const noteBodySummaryElement = notecard.element.querySelector('.notecard-summary-body');
-  const noteTopicElement = notecard.element.querySelector('.notecard-topic');
+// function createElement(notecard){
+//   const template = document.querySelector('#notecard-template');
+//   const clone = template.content.cloneNode(true);
+//   notecard.element = clone.querySelector('.notecard-container');
+//   const notecardListElement = document.querySelector('#notecard-list');
+//   notecardListElement.append(clone);
+//   updateElement(notecard);
+// }
 
-  noteImageElement.src = notecard.noteImageURL;
-  noteImageElement.alt = notecard.noteImageText;
-  noteTitleElement.innerText = notecard.noteTitle;
-  noteBodySummaryElement.innerText = notecard.noteSummaryBody;
-  noteTopicElement.innerText = notecard.noteTopic;
-}
+// function updateElement(notecard){
+//   const noteImageElement = notecard.element.querySelector('.notecard-image');
+//   const noteTitleElement = notecard.element.querySelector('.notecard-title');
+//   const noteBodySummaryElement = notecard.element.querySelector('.notecard-summary-body');
+//   const noteTopicElement = notecard.element.querySelector('.notecard-topic');
+
+//   noteImageElement.src = notecard.noteImageURL;
+//   noteImageElement.alt = notecard.noteImageText;
+//   noteTitleElement.innerText = notecard.noteTitle;
+//   noteBodySummaryElement.innerText = notecard.noteSummaryBody;
+//   noteTopicElement.innerText = notecard.noteTopic;
+// }
