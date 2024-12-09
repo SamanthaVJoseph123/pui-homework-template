ced1b00b7f27382c31358a80bbcc072fbedf279a
SamanthaVJoseph123 <svjoseph@andrew.cmu.edu>
2024-12-09 00:12:42 -0500

    fixed JS file

--- final-project/app.js
+++ final-project/app.js
@@ -14,68 +14,60 @@ let notecardDictionaryString = JSON.parse(x);
 
 let topicCounts = [];
 
-if (Object.keys(notecardDictionaryString).length > 0){
-    for (let i=0; i < topics.length; i++) {
-        const currentTopic = topics[i];
-        let count = 0;
+for (let i=0; i < topics.length; i++) {
+    const currentTopic = topics[i];
+    let count = 0;
 
-        for (const key in notecardDictionaryString) {
-            const note = notecardDictionaryString[key];
-            if (note.noteTopic == currentTopic){
-                count++;
-            }
-        }
-
-        if (count > 0){
-            topicCounts.push({topic: currentTopic, count: count});
+    for (const key in notecardDictionaryString) {
+        const note = notecardDictionaryString[key];
+        if (note.noteTopic === currentTopic){
+            count++;
         }
     }
-} else {
-    topicCounts = [];
-}
+
+    topicCounts.push({topic: currentTopic, count: count });
+    }
 
 // var dataset = [80,100,56,120,180,30,40,120,160];
 
-const svgWidth = 500, svgHeight = 300, barPadding = 5;
+const svgWidth = 800, svgHeight = 250, barPadding = 5;
 
-if (topicCounts.length > 0){
-    barWidth = svgWidth / topicCounts.length;
-} else {
-    barWidth = 0;
-}
+console.log("I AM HERE NOW!!");
+console.log(topicCounts);
+console.log(topicCounts.length);
+const barWidth = (svgWidth / topicCounts.length);
+
+// if (topicCounts.length > 0){
+//     barWidth = svgWidth / topicCounts.length;
+// } else {
+//     barWidth = 0;
+// }
 
 const svg = d3.select('#bar-chart')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+
 svg.selectAll("*").remove();
 
-if (topicCounts.length > 0){
-    svg.selectAll("rect")
-        .data(topicCounts)
-        .enter()
-        .append("rect")
-        .attr("y", d => svgHeight - d.count * 20)
-        .attr("height", d => d.count * 20)
-        .attr("width", barWidth - barPadding)
-        .attr("x", (d,i) => i * barWidth)
-        .attr("fill", "#69b3a2");
-    
-    svg.selectAll("text")
-        .data(topicCounts)
-        .enter()
-        .append("text")
-        .attr("x", (d,i) => i * barWidth + (barWidth / 2))
-        .attr("y", svgHeight - 5)
-        .attr("text-anchor", "middle")
-        .text(d => d.topic)
-        .style("font-size", "10px");
-} else {
-    svg.append("text")
-        .attr("x", svgWidth / 2)
-        .attr("y", svgHeight / 2)
-        .attr("text-anchor", "middle")
-        .text("No posts available")
-        .style("font-size", "16px")
-        .style("fill", "#666");
-}
+
+svg.selectAll("rect")
+    .data(topicCounts)
+    .enter()
+    .append("rect")
+    .attr("y", d => svgHeight - d.count * 20)
+    .attr("height", d => d.count * 20)
+    .attr("width", barWidth - barPadding)
+    .attr("x", (d,i) => i * barWidth)
+    .attr("fill", "#69b3a2");
+
+svg.selectAll("text")
+    .data(topicCounts)
+    .enter()
+    .append("text")
+    .attr("x", (d,i) => i * barWidth + (barWidth / 2))
+    .attr("y", svgHeight - 5)
+    .attr("text-anchor", "middle")
+    .text(d => d.topic)
+    .style("font-size", "15px")
+    .style("font-family", "'Source Serif 4', serif");
